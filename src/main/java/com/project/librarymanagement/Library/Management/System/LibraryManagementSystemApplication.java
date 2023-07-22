4b1bdd5455499ed5033d629bfb7e5095e7fe1393
bikashdas1 <bikashdas2899@gmail.com>
2023-07-23 00:09:54 +0530

    Adding more redundant code.

--- src/main/java/com/project/librarymanagement/Library/Management/System/LibraryManagementSystemApplication.java
+++ src/main/java/com/project/librarymanagement/Library/Management/System/LibraryManagementSystemApplication.java
@@ -2,6 +2,11 @@ package com.project.librarymanagement.Library.Management.System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.project.librarymanagement.Library.Management.dao.AppDAO;
+import com.project.librarymanagement.Library.Management.entity.User;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
@@ -9,6 +14,15 @@ public class LibraryManagementSystemApplication {
 	public static void main(String[] args) {
 		// The main starting point of the application
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+		System.out.println("\nSpring application started!");
+		//		createNewUser(dao);
+	}
+	
+	public static void createNewUser(AppDAO dao) {
+		User user1 = new User("Bikash");
+		dao.save(user1);
 	}
 
+	
+	
 }
